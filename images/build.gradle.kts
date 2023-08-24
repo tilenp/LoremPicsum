744768a8435f44ad482eb6ed9241c42dd8f838bf
tilen <tilen.prepeluh@heetch.com>
2023-08-24 05:28:24 +0200

    images module dependencies

--- images/build.gradle.kts
+++ images/build.gradle.kts
@@ -40,32 +40,57 @@ android {
     }
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 dependencies {
 
     implementation(project(":core"))
+    implementation(project(":domain"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    /** Coroutines **/
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
+    /** Coil **/
+    implementation("io.coil-kt:coil-compose:2.2.2")
+
+    /** Compose **/
+    implementation("androidx.navigation:navigation-compose:2.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-alpha03")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    /** Compose **/
-    implementation("androidx.navigation:navigation-compose:2.7.0")
 
     /** Hilt **/
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    /** JUnit 5 **/
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+
+    /** MockK **/
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("io.mockk:mockk-agent-jvm:1.13.4")
+
+    /** Turbine **/
+    testImplementation("app.cash.turbine:turbine:0.11.0")
+
+    /** ViewModel **/
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
 }
 
 kapt {
